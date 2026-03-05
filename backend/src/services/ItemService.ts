0a83d4b6b4a9525e6136d1e6456b35f060df714b
nick <nicholascezarmaiafernandezmaia@gmail.com>
2026-03-05 20:09:49 -0300

    adicionando gitignore

--- backend/src/services/ItemService.ts
+++ backend/src/services/ItemService.ts
@@ -1,23 +1,23 @@
-import { Item } from "../models/Item"
-import { v4 as uuid} from 'uuid'
+import { Item } from "../models/Item";
+import { v4 as uuid } from "uuid";
 
-class ItemService{
-    private items: Item[] = [];
+class ItemService {
+  private items: Item[] = [];
 
-    getAll(){
-        return this.items;
-    }
+  getAll() {
+    return this.items;
+  }
 
-    create(nome:string, quantidade: number,categoria: string) {
-        const newItem: Item= {
-            id: uuid(),
-            nome,
-            quantidade,
-            categoria,
-            status: 'disponivel'
-        };
-        this.items.push(newItem)
-        return newItem;
-    }
+  create(nome: string, quantidade: number, categoria: string) {
+    const newItem: Item = {
+      id: uuid(),
+      nome,
+      quantidade,
+      categoria,
+      status: "disponivel",
+    };
+    this.items.push(newItem);
+    return newItem;
+  }
 }
 export const itemService = new ItemService();
